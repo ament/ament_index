0dba2f42c7f9b18a676c96d5643f26d5213513dd
Steven! Ragnarök <steven@nuclearsandwich.com>
2019-04-11 12:16:26 -0700

    0.7.0

--- ament_index_python/setup.py
+++ ament_index_python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ament_index_python',
-    version='0.5.1',
+    version='0.7.0',
     packages=find_packages(exclude=['test']),
     install_requires=['setuptools'],
     zip_safe=True,
