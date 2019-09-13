b5ca61ff32183a5c2f972b00d16358a945eaa51e
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-09-13 16:28:45 -0700

    install package manifest (#34)
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- ament_index_python/setup.py
+++ ament_index_python/setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages
 from setuptools import setup
 
+package_name = 'ament_index_python'
+
 setup(
-    name='ament_index_python',
+    name=package_name,
     version='0.7.0',
     packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     author='Dirk Thomas',
