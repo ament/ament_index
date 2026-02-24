c9186fd29284c566990ac47694594c9aed70e593
Tim Clephas <tim.clephas@nobleo.nl>
2026-02-24 13:53:52 +0100

    Fix include
    Signed-off-by: Tim Clephas <tim.clephas@nobleo.nl>

--- ament_index_cpp/src/get_package_share_path.cpp
+++ ament_index_cpp/src/get_package_share_path.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "ament_index_cpp/get_package_share_directory.hpp"
+#include "ament_index_cpp/get_package_share_path.hpp"
 
 #include <filesystem>
 #include <string>
